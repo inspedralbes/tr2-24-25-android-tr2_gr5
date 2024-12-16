1623217fc062a7b4cbca900ae99fab38ac462113
a21xavmarvel <a21xavmarvel@inspedralbes.cat>
2024-12-16 09:05:36 +0100

    VALORACION FUNCIONAL

--- app/src/main/java/com/example/supportly/ui/view/Valoracio.kt
+++ app/src/main/java/com/example/supportly/ui/view/Valoracio.kt
@@ -1,17 +1,23 @@
-package com.example.supportly.ui.view
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextAlign // Import correcto
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.foundation.Image
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.graphics.painter.Painter
 import com.example.supportly.R
 
 @Composable
@@ -32,7 +38,7 @@ fun MentorRatingsScreen() {
 
         // Título
         Text(
-            text = "VALORACIONS DE MENTOR",
+            text = "VALORACIONES DE MENTOR",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
@@ -65,7 +71,7 @@ fun MentorRatingsScreen() {
                 Text(text = "Estadísticas, profes con mejor valoración, etc", color = Color.White)
             }
             Button(onClick = { /* Acción Petición */ }) {
-                Text(text = "PETICIÓ", color = Color.White)
+                Text(text = "PETICIÓN", color = Color.White)
             }
         }
     }
