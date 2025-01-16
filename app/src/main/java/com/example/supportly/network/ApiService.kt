0152008aec05667df3c591e689459d33eb943058
Edson <a23edstorcev@inspedralbes.cat>
2025-01-16 01:33:18 +0100

    f

--- app/src/main/java/com/example/supportly/network/ApiService.kt
+++ app/src/main/java/com/example/supportly/network/ApiService.kt
@@ -22,7 +22,7 @@ import retrofit2.http.Query
 
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "http://tr2g5.dam.inspedralbes.cat:23412/"
 //http://tr2g5.dam.inspedralbes.cat:23412
 
     private val retrofit by lazy {
